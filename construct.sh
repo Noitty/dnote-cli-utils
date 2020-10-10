def22e6006f6f193a39328586573bba65a50b73a
noitty <joan.adria@gmail.com>
2020-10-10 17:06:10 +0200

    removed some comments

--- construct.sh
+++ construct.sh
@@ -5,25 +5,25 @@ env_path=~/.dnote-cli-utils
 # create books/notes and configure .env file
 mkdir -p $env_path
 touch $env_path/.env
-#dnote add all-tasks
+dnote add all-tasks
 noteid=$(dnote view all-tasks | awk -F ")" '{print $1}' | awk -F "(" '{print $2}' | sed '/^\s*$/d')
 echo "export ALL_TASKS=${noteid}" >> $env_path/.env
-#dnote add done-tasks
+dnote add done-tasks
 noteid=$(dnote view done-tasks | awk -F ")" '{print $1}' | awk -F "(" '{print $2}' | sed '/^\s*$/d')
 echo "export DONE_TASKS=${noteid}" >> $env_path/.env
-#dnote add today-tasks
+dnote add today-tasks
 noteid=$(dnote view today-tasks | awk -F ")" '{print $1}' | awk -F "(" '{print $2}' | sed '/^\s*$/d')
 echo "export TODAY_TASKS=${noteid}" >> $env_path/.env
-#dnote add weekly-progress
+dnote add weekly-progress
 noteid=$(dnote view weekly-progress | awk -F ")" '{print $1}' | awk -F "(" '{print $2}' | sed '/^\s*$/d')
 echo "export WEEKLY_PROGRESS=${noteid}" >> $env_path/.env
 source $env_path/.env
 
 # Write the first text in the notes
-#dnote edit $TODAY_TASKS -c "Here you can see your diary tasks:"
-#dnote edit $DONE_TASKS -c "Here you have all the tasks that you have already accomplish:"
-#dnote edit $ALL_TASKS -c "Here you have all the tasks that you must accomplish:"
-#dnote edit $WEEKLY_PROGRESS -c "Here you will find all the tasks performed in the last week:"
+dnote edit $TODAY_TASKS -c "Here you can see your diary tasks:"
+dnote edit $DONE_TASKS -c "Here you have all the tasks that you have already accomplish:"
+dnote edit $ALL_TASKS -c "Here you have all the tasks that you must accomplish:"
+dnote edit $WEEKLY_PROGRESS -c "Here you will find all the tasks performed in the last week:"
 
 # Final comments
 echo "dnote-cli-utils correctly configured! Remember that in ~/.dnote/dnoterc you can modify the default editor and the remote server url, and in ~/.dnote-cli-utils/.env the note ids."
